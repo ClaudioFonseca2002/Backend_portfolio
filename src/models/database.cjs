abf4db9e70ecb8cde4552b5c6e7d6c842ccbb1f9
Fonseca Claudio <fonsecaclaudioroman2000@gmail.com>
2024-08-15 13:49:09 -0300

    Se agrego database.cjs

---
+++ src/models/database.cjs
@@ -0,0 +1,21 @@
+const mysql = require('mysql');
+require('dotenv').config();
+
+// Configura la conexión a la base de datos
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+
+// Conecta a la base de datos
+connection.connect(err => {
+  if (err) {
+    console.error('Error connecting to the database:', err.stack);
+    return;
+  }
+  console.log('Connected to the database as id ' + connection.threadId);
+});
+
+module.exports = connection;
